3c5dec528eeb082c383fea9929510090bfc3b9c9
Sami Santos <sami.santos@ucsal.edu.br>
2026-08-13 13:22:35 -0300

    feat: cria novas moedas no GerenciadorMoedas e adicionar Moedas ao  ArrayListt

---
+++ src/service/GerenciadorMoedas.java
@@ -0,0 +1,22 @@
+package service;
+import model.Moeda;
+
+import java.util.ArrayList;
+public class GerenciadorMoedas {
+
+    ArrayList<Moeda> moedas = new ArrayList<>();
+
+    Moeda euro = new Moeda("Euro", "EUR", '€');
+    Moeda Dólar = new Moeda("Dólar", "USD", '$');
+    Moeda DólarAustraliano = new Moeda("DólarAustraliano", "A$", '$');
+
+
+    public GerenciadorMoedas() {
+        moedas.add(euro);
+        moedas.add(Dólar);
+        moedas.add(DólarAustraliano);
+
+
+        }
+    }
+
