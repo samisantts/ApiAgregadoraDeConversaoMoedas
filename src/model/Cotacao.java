43ef70c4c4d02021a4763a00402f9a2704cd0573
Sami Santos <sami.santos@ucsal.edu.br>
2026-08-27 14:38:51 -0300

    feat: implementa classe de cotação e seus getters

---
+++ src/model/Cotacao.java
@@ -0,0 +1,27 @@
+package service;
+
+import model.Moeda;
+public class Cotacao {
+
+    private Moeda moedaDestino;
+    private Moeda moedaOrigem;
+    private  Double valor;
+
+    public Cotacao(Moeda moedaDestino, Moeda moedaOrigem, double valor){
+        this.moedaDestino = moedaDestino;
+        this.moedaOrigem = moedaOrigem;
+        this.valor = valor;
+    }
+        public Moeda getMoedaDestino(){
+          return moedaDestino;
+}
+      public Moeda getMoedaOrigem(){
+        return moedaOrigem;
+
+      }
+
+      public Double getValor(){
+        return valor;
+      }
+
+}
