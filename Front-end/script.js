d3df0b503cb03ce6de2cb917ca7e8d478fc73891
samisantts <sami.santos@ucsal.edu.br>
2026-09-05 20:40:30 -0300

    Adiciona interacao para inverter moedas

---
+++ Front-end/script.js
@@ -0,0 +1,11 @@
+const  moedaOrigem = document.getElementById("mmoedaOrigem")
+const moedaDestino = document.getElementById("moedaDestino")
+const  inveter = document.getElementById ("inveter")
+
+let temporario = moedaOrigem.value 
+
+botaoInverter.addEventListener("click", () => {
+
+
+
+});
