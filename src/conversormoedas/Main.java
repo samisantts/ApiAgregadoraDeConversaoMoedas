ff6544ae327ad51ccde9a0a23be6135c85644e0f
samisantts <sami.santos@ucsal.edu.br>
2026-09-10 14:13:21 -0300

    feat: spring inicializado

--- src/conversormoedas/Main.java
+++ src/conversormoedas/Main.java
@@ -1,35 +1,12 @@
 package conversormoedas;
-import java.util.Scanner;
-import model.Moeda;
-import service.BuscarCotacoes;
-import service.Cotacao;
-import service.GerenciadorMoedas;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        GerenciadorMoedas gerenciador = new GerenciadorMoedas();
-
-        System.out.println("Digite a sigla da moeda de origem:");
-        String siglaOrigem = sc.nextLine();
-
-        System.out.println("Digite a sigla da moeda de destino:");
-        String siglaDestino = sc.nextLine();
-
-        Moeda moedaOrigem = gerenciador.buscarMoeda(siglaOrigem);
-        Moeda moedaDestino = gerenciador.buscarMoeda(siglaDestino);
-
-        BuscarCotacoes cotacoes = new BuscarCotacoes();
-
-        Cotacao cotacao = cotacoes.buscarCotacao(moedaDestino, moedaOrigem);
-
-        System.out.println("Moeda de origem: " + moedaOrigem.getSigla());
-        System.out.println("Moeda de destino: " + moedaDestino.getSigla());
-        System.out.println("Cotação: " + cotacao.getValor());
-
-        sc.close();
+        SpringApplication.run(Main.class, args);
     }
 }
