a7668608b07c853cddf895712b8dbf6249dc37fc
samisantts <sami.santos@ucsal.edu.br>
2026-09-26 14:28:58 -0300

    feat: adiciona estrutura inicial para conversão e busca de cotações

--- src/main/java/conversormoedas/service/BuscarCotacoes.java
+++ src/main/java/conversormoedas/service/BuscarCotacoes.java
@@ -1,8 +1,16 @@
 package conversormoedas.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class BuscarCotacoes {
 
+    private final RestClient restClient;
+
+    public BuscarCotacoes(RestClient.Builder builder) {
+        this.restClient = builder
+                .baseUrl("https://api.frankfurter.dev")
+                .build();
+    }
 }
--- src/main/java/conversormoedas/service/ConversaoMoedas.java
+++ src/main/java/conversormoedas/service/ConversaoMoedas.java
@@ -1,8 +1,44 @@
 package conversormoedas.service;
 
 import org.springframework.stereotype.Service;
+import conversormoedas.model.Moeda;
 
 @Service
 public class ConversaoMoedas {
 
-}
+        private final GerenciadorMoedas gerenciadorMoedas;
+        private final BuscarCotacoes buscarCotacoes;
+
+        public ConversaoMoedas(
+                GerenciadorMoedas gerenciadorMoedas,
+                BuscarCotacoes buscarCotacoes) {
+
+            this.gerenciadorMoedas = gerenciadorMoedas;
+            this.buscarCotacoes = buscarCotacoes;
+        }
+
+        public double converter(double valor, String siglaOrigem, String siglaDestino) {
+
+            Moeda moedaOrigem = gerenciadorMoedas.buscarMoeda(siglaOrigem);
+            Moeda moedaDestino = gerenciadorMoedas.buscarMoeda(siglaDestino);
+
+            if (moedaOrigem == null || moedaDestino == null) {
+                throw new IllegalArgumentException("Moeda não encontrada.");
+            }
+
+            if (valor < 0) {
+                throw new IllegalArgumentException("O valor não pode ser negativo.");
+            }
+
+            double cotacao = buscarCotacoes.buscarCotacoes(
+                    moedaOrigem,
+                    moedaDestino
+            );
+
+            return valor * cotacao;
+
+
+        }
+    }
+
+
